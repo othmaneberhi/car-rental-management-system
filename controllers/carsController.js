e3c686ff3137e3ef4e754460cad737130811b8c9
Othmane berhi <omaxnoti@gmail.com>
2023-05-22 19:19:06 +0000

    Implement finding available cars

--- controllers/carsController.js
+++ controllers/carsController.js
@@ -76,6 +76,29 @@ exports.findCarById = async (req,res)=>{
         })
 
     }catch(error){
+        res.status(500).json({
+            status:500,
+            success:false,
+            error:{
+                code:error.code,
+                message:error.message
+            },
+        })
+    }
+}
+
+exports.findAvailableCars = async (req,res)=>{
+    try{
+        console.log("availabale api")
+        const cars = await Car.findAll({where:{status:1}})
+        res.status(200).json({
+            status:200,
+            success:true,
+            data:{
+                cars:cars
+            }
+        })
+    }catch(error){
         console.log(error)
         res.status(500).json({
             status:500,
--- routes/cars.js
+++ routes/cars.js
@@ -5,6 +5,8 @@ const {protectRoute} = require('../middlewares/authMiddleware')
 
 /* GET cars listing. */
 router.get('/',protectRoute,carsController.findAllCars)
+router.get('/available',protectRoute,carsController.findAvailableCars)
 router.get('/:id',protectRoute,carsController.findCarById)
+
 
 module.exports = router;
